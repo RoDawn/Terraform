081fc933ed04739f9b8c912dd8c594dadd142d1a
RoDawn <143478463+RoDawn@users.noreply.github.com>
2024-02-20 10:16:17 +0900

    Add files via upload

---
+++ Day1/vpc.tf
@@ -0,0 +1,14 @@
+#Provider 설정
+provider "ncloud" {
+  support_vpc = true
+  region      = "KR"
+  access_key  = var.access_key
+  secret_key  = var.secret_key
+}
+
+
+# VPC 설정
+resource "ncloud_vpc" "vpc" {
+  name            = var.test #vpc 이름 지정
+  ipv4_cidr_block = "10.0.0.0/16"
+}
